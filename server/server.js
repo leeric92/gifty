cbb88ac2fea8c107484c4c926fd5dadbb9dae6ec
leeric92 <hle2806@uw.edu>
2015-08-26 16:14:14 -0700

    finish testing with Amazon API

---
+++ server/server.js
@@ -0,0 +1,15 @@
+var aws = require("aws-lib");
+
+var prodAdv = aws.createProdAdvClient('AKIAIT6MPAH4YTKJ46BA', 'TJlF9dNeQS7ZAn4g6NwWGqRvbp16xvBEyTNAGrSq', 'eric0e7-20');
+
+// console.log(">>>>>>>>>>>prodAdv",prodAdv.client.call)
+
+ var options = {ItemId: '1118531647', ResponseGroup: 'Offers, ItemAttributes, Images'}
+
+// var options = {SearchIndex: "Books", Keywords: "Javascript"}
+
+prodAdv.call("SimilarityLookup", options, function(err, result) {
+  // console.log(">>>>>>>>>>",result.Items.Item[0].ItemLinks);
+  // console.log(">>>>>>>>>>",result.Items.Item[0]);
+  console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>",result.Items.Item);
+})
