7fd1880620581ad4ebfb5d1cf1ed78c6461ae667
leeric92 <hle2806@uw.edu>
2015-08-27 19:00:11 -0700

    finish with webpack barebone

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,9 @@
 var path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'client/app/main.js'),
+  entry:  ['webpack/hot/dev-server', path.resolve(__dirname, 'client/app/main.js')],
   output: {
     path: path.resolve(__dirname, 'client/build'),
     filename: 'bundle.js'
-  },
-  module {
-    loaders: []
   }
 };
