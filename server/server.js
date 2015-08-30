66a3a6a833521ca5f19642637e3990bbdf1bdf79
leeric92 <hle2806@uw.edu>
2015-08-29 17:38:33 -0700

    modifying server.js

--- server/server.js
+++ server/server.js
@@ -6,15 +6,17 @@ var prodAdv = aws.createProdAdvClient('AKIAIT6MPAH4YTKJ46BA', 'TJlF9dNeQS7ZAn4g6
 
  // var options = {ItemId: '1118531647', ResponseGroup: 'Offers, ItemAttributes, Images'}
 
-var options = {SearchIndex: "Books", Keywords: "Javascript", ResponseGroup: 'Offers, ItemAttributes, Images'}
+var options = {SearchIndex: "Books", Keywords: "Javascript", ResponseGroup: 'Offers, ItemAttributes, Images, OfferSummary, PromotionSummary'}
 
 // prodAdv.call("SimilarityLookup", options, function(err, result) {
 prodAdv.call("ItemSearch", options, function(err, result) {
   // console.log(">>>>>>>>>>",result.Items.Item[0].ItemLinks);
   // console.log(">>>>>>>>>>",result.Items.Item[0]);
-  console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>",result.Items.Item[0].ItemLinks);
-
-
+  console.log('#####################################')
+  result.Items.Item.forEach(function(item){
+    console.log(item);
+  });
   //get the price 
   //console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>",result.Items.Item[0].OfferSummary.LowestNewPrice.FormattedPrice);
 })
+
